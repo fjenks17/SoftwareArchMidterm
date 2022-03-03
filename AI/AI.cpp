9dd8c21eae80de3e2a4958ead203bcb722cd2049
fjenks17 <fjenks17@gmail.com>
2022-03-03 12:52:17 -0500

    removing refs to old names

--- AI/AI.cpp
+++ AI/AI.cpp
@@ -13,14 +13,17 @@ void AI_Spawn(double x, double y, double z) {
 bool AI_Attack(bool inRange, bool pc_Visible) {
 	//do something
 	cout << "AI Attacks" << endl;
+	return true;
 }
 
 double AI_Move_X(bool canMove, double target_Loc) {
 	//do something
 	cout << "AI Moves North/South" << endl;
+	return 0;
 }
 
 double AI_Move_Z(bool canMove, double target_Loc) {
 	//do something
 	cout << "AI Moves East/West" << endl;
+	return 0;
 }
--- Application/Application.cpp
+++ Application/Application.cpp
@@ -13,14 +13,17 @@ void Pickup_Item(int item_id) {
 bool PC_Attack(int AI_ID, int damage) {
 	//do something
 	cout << "PC Attacks" << endl;
+	return true;
 }
 
 double PC_Move_X(bool canMove) {
 	//do something
 	cout << "PC Moves North/South" << endl;
+	return 0;
 }
 
 double PC_Move_Z(bool canMove) {
 	//do something
 	cout << "PC Moves East/West" << endl;
+	return 0;
 }
